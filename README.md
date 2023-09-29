e0e6189e11bf2e7859f4055bf27d32dd30696add
Kristopher Overholt <koverholt@google.com>
2023-09-28 22:28:14 -0500

    Initial commit of site

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ask-does
+
 Web app to search and chat with documents from DevOps Enterprise Summit 2023 website and schedule
---
+++ src/routes/chat/+page.svelte
@@ -0,0 +1,22 @@
+<div>
+<script src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"></script>
+<df-messenger
+  project-id="dora-chat-bot"
+  agent-id="350c4616-bd51-48d1-8192-6408c4a5aada"
+  language-code="en"
+  storage-option="none">
+  <df-messenger-chat
+   chat-title="Ask DOES">
+  </df-messenger-chat>
+</df-messenger>
+<style>
+  df-messenger {
+    z-index: 999;
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    top: 0;
+    width: 350px;
+  }
+</style>
+</div>
